942e6fddb11caec641d113db556f2c88c0a459c6
ValeriiPetriuk <vpetruk257@gmail.com>
2025-05-17 17:45:59 +0300

    add auth

--- chatbot_frontend/src/main.py
+++ chatbot_frontend/src/main.py
@@ -1,9 +1,32 @@
 import os
 import requests
 import streamlit as st
+from streamlit_jwt_authenticator import Authenticator
 
 CHATBOT_URL = os.getenv("CHATBOT_URL", "http://localhost:8000/bike-rag-agent")
+LOGIN_URL = "http://chatbot_api:8000/user/login"
+if "token" not in st.session_state:
+    st.session_state.token = None
 
+def login():
+    st.subheader("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        response = requests.post(LOGIN_URL, data={
+            "username": username,
+            "password": password
+        })
+        if response.status_code == 200:
+            st.session_state.token = response.json()["access_token"]
+            st.success("Logged in!")
+            st.rerun()
+        else:
+            st.error("Invalid credentials")
+
+if st.session_state.token is None:
+    login()
+    st.stop()
 with st.sidebar:
     st.header("About the Bike Store Assistant")
     st.markdown(
@@ -49,10 +72,11 @@ if prompt := st.chat_input("What do you want to know?"):
 
     st.session_state.messages.append({"role": "user", "output": prompt})
 
-    data = {"text": prompt}
+    data = {"text": prompt, }
+    headers = {"Authorization": f"Bearer {st.session_state.token}"}
 
     with st.spinner("Searching for an answer..."):
-        response = requests.post(CHATBOT_URL, json=data)
+        response = requests.post(CHATBOT_URL, json=data, headers=headers)
 
         if response.status_code == 200:
             output_text = response.json()["output"]
